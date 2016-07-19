3f48fc893cd2c54de571f238c860eb958c820d3f
Jorge Figueroa <yknx.4.b@gmail.com>
2016-07-19 15:35:11 -0500

    Create network helper classes

---
+++ app/src/main/java/com/yknx4/notificationtracker/network/LoginService.kt
@@ -0,0 +1,11 @@
+package com.yknx4.notificationtracker.network
+
+/**
+ * Created by yknx4 on 7/19/16.
+ */
+
+class LoginService{
+
+
+
+}
---
+++ app/src/main/java/com/yknx4/notificationtracker/services/TokenRefresher.java
@@ -0,0 +1,10 @@
+package com.yknx4.notificationtracker.services;
+
+/**
+ * Created by yknx4 on 7/19/16.
+ */
+public class TokenRefresher {
+    public static void refreshToken(){
+
+    }
+}
